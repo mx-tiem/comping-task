a97bf09b356efcef001d62383ed4fd2892281457
mx-tiem <mx.tiem@gmail.com>
2025-06-25 12:44:51 +0200

    adds initial screen for selecting task

--- src/app/select-task/select-task.ts
+++ src/app/select-task/select-task.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-select-task',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class SelectTask {
 
+  constructor(private router: Router) { }
+
+  goToTask(task: string) {
+    this.router.navigate([task]);
+  }
 }
